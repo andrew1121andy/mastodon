53d7bc8cd127fc0d36277ff925705974e1551c31
andrew1121andy <andybeat.1121@gmail.com>
2021-06-26 08:10:07 +0900

    P84 リスト09.6 エラー名：Unknown host'dl.google.com'

--- app/src/main/java/io/keiji/sample/mastodonclient/MainFragment.kt
+++ app/src/main/java/io/keiji/sample/mastodonclient/MainFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import io.keiji.sample.mastodonclient.databinding.FragmentMainBinding
 import retrofit2.Retrofit
 import android.util.Log
@@ -11,6 +13,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
@@ -19,8 +22,12 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         private const val API_BASE_URL = "https://androidbook2020.keiji.io"
     }
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
     private val retrofit = Retrofit.Builder()
         .baseUrl(API_BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
     private val api = retrofit.create(MastodonApi::class.java)
 
@@ -33,11 +40,8 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         binding?.button?.setOnClickListener {
             binding?.button?.text = "clicked"
             CoroutineScope(Dispatchers.IO).launch {
-                val response = api.fetchPublicTimeline().string()
-                Log.d(TAG, response)
-                withContext(Dispatchers.Main) {
-                    binding?.button?.text = response
-                }
+                val tootList = api.fetchPublicTimeline()
+                showTootList(tootList)
             }
         }
     }
@@ -46,5 +50,14 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         super.onDestroyView()
 
         binding?.unbind()
+
+    private suspend fun showTootList(
+        tootList: List<Toot>
+    ) = withContext(Dispatchers.Main) {
+        val binding = binding ?: return@withContext
+        val accountNameList = tootList.map { it.account.displayName }
+        binding.button.text = accountNameList.joinToString ("\n")
+    }
+
     }
 }
