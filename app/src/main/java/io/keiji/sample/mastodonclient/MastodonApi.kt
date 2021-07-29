74aac9f06f6e76ca5b6e3319e493417c607b4fbf
andrew1121andy <andybeat.1121@gmail.com>
2021-07-29 09:56:05 +0900

    151 リスト23.4

--- app/src/main/java/io/keiji/sample/mastodonclient/MastodonApi.kt
+++ app/src/main/java/io/keiji/sample/mastodonclient/MastodonApi.kt
@@ -1,6 +1,7 @@
 package io.keiji.sample.mastodonclient
 
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface MastodonApi {
@@ -8,5 +9,11 @@ interface MastodonApi {
     suspend fun fetchPublicTimeline(
         @Query("max_id") maxId: String? = null,
         @Query("only_media") onlyMedia: Boolean = false
+    ): List<Toot>
+
+    @GET("api/vl/timelines/home")
+    suspend fun fetchHomeTimeline(
+        @Header("Authorization") accessToken: String,
+        @Query("max_id") maxId: String? = null
     ): List<Toot>
 }
